8aecfc329bbd3334eb768649c0c131572e2b7536
Zak Wilson <zak.wilson@gmail.com>
2012-09-16 18:39:53 +0200

    Sector images to find sharp regions - compensates for depth of field

--- src/fuzzbuster/core.clj
+++ src/fuzzbuster/core.clj
@@ -16,75 +16,126 @@
   (sobel-edge-detection (ImageIO/read (j/as-file f))))
 
 (defn light-or-dark [^BufferedImage img x y]
-  (if (< 127 (rgb-to-luminance (.getRGB img x y)))
-    :light
-    :dark))
+  (try 
+   (if (< 127 (rgb-to-luminance (.getRGB img x y)))
+     :light
+     :dark)
+   (catch Exception _
+     (throw (Exception. (str x " " y))))))
 
-(defn edge-count [^BufferedImage img place dir]
-  (loop [results {:count 0 :width 0}
-         pos 0
-         last nil]
-    (if (> (if (= dir :horiz)
-             (.getWidth img)
-             (.getHeight img))
-           pos)
-      (if-not (= last (if (= dir :horiz)
-                        (light-or-dark img pos place)
-                        (light-or-dark img place pos))) ; this is an edge
-        (recur (-> results
-                   (update-in [:count] + 1)
-                   (update-in [:width] + (if (= :light last)
-                                           1
-                                           0)))
-               (inc pos)
-               (if (= :light last)
-                 :dark
-                 :light))
-        (recur (if (= :dark last)
-                 (update-in results [:width] + 1)
-                 results)
-               (inc pos)
-               last))
-      results)))
+(defn edge-count 
+  ([^BufferedImage img place dir]
+     (edge-count img place dir 0 (.getWidth img) 0 (.getHeight img)))
+  ([^BufferedImage img place dir start end]
+     (loop [results {:count 0 :width 0}
+            pos start
+            last nil]
+       (if (and (>= (if (= dir :horiz)
+                      (min (.getWidth img) end)
+                      (min (.getHeight img) end))
+                    (inc pos)) ; the last px is one less than width/height
+                (>= (if (= dir :horiz)
+                     (.getHeight img)
+                     (.getWidth img))
+                   (inc place)))
+         (if-not (= last (if (= dir :horiz)
+                           (light-or-dark img pos place)
+                           (light-or-dark img place pos))) ; this is an edge
+           (recur (-> results
+                      (update-in [:count] + 1)
+                      (update-in [:width] + (if (= :light last)
+                                              1
+                                              0)))
+                  (inc pos)
+                  (if (= :light last)
+                    :dark
+                    :light))
+           (recur (if (= :dark last)
+                    (update-in results [:width] + 1)
+                    results)
+                  (inc pos)
+                  last))
+         results))))
 
-(defn edge-count-horiz [^BufferedImage img]
-  (apply merge-with +
-         (for [y (range (.getHeight img))]
-           (edge-count img y :horiz))))
+(defn edge-count-horiz 
+  ([^BufferedImage img]
+     (apply merge-with +
+            (for [y (range (.getHeight img))]
+              (edge-count img y :horiz))))
+  ([^BufferedImage img start-x end-x start-y end-y]
+     (apply merge-with +
+            (for [y (range start-y end-y)]
+              (edge-count img y :horiz start-x end-x)))))
 
-(defn edge-count-vert [^BufferedImage img]
-  (apply merge-with +
-         (for [x (range (.getWidth img))]
-           (edge-count img x :vert))))
+(defn edge-count-vert
+  ([^BufferedImage img]
+     (apply merge-with +
+            (for [y (range (.getWidth img))]
+              (edge-count img y :vert))))
+  ([^BufferedImage img start-x end-x start-y end-y]
+     (apply merge-with +
+            (for [x (range start-x end-x)]
+              (edge-count img x :horiz start-y end-y)))))
 
-(defn edge-width-horiz [img]
-  (let [{:keys [width count]} (edge-count-horiz img)]
-    (/ width count)))
+(defn edge-width-horiz 
+  ([^BufferedImage img]
+     (edge-width-horiz img 0 (.getWidth img) 0 (.getHeight img)))
+  ([img start-x end-x start-y end-y]
+     (let [{:keys [width count]} (edge-count-horiz img start-x end-x start-y end-y)]
+       (/ width (max count 1)))))
 
-(defn edge-width-vert [img]
-  (let [{:keys [width count]} (edge-count-vert img)]
-    (/ width count)))
+(defn edge-width-vert
+  ([^BufferedImage img]
+     (edge-width-vert img 0 (.getWidth img) 0 (.getHeight img)))
+  ([img start-x end-x start-y end-y]
+     (let [{:keys [width count]} (edge-count-vert img start-x end-x start-y end-y)]
+       (/ width (max count 1)))))
 
-(defn edge-width-img [img]
-  (/ (+ (edge-width-horiz img)
-        (edge-width-vert img))
-     2))
+(defn mean [& numbers]
+  (/ (apply + numbers)
+     (count numbers)))
 
-(defn edge-width-file [f]
-  (edge-width-img (ImageIO/read (j/as-file f))))
+(defn edge-width-img 
+  ([img]
+     (edge-width-img img 1))
+  ([^BufferedImage img sectors]
+     (let [sector-count sectors
+           sector-width (Math/ceil (/ (.getWidth img) sector-count))
+           sector-height (Math/ceil (/ (.getHeight img) sector-count))
+           ]
+       (for [x-mult (range sector-count)
+             y-mult (range sector-count)]
+         (mean (edge-width-horiz img
+                                 (* sector-width x-mult) (* sector-width (inc x-mult))
+                                 (* sector-height y-mult) (* sector-height (inc y-mult)))
+               (edge-width-vert img
+                                (* sector-width x-mult) (* sector-width (inc x-mult))
+                                (* sector-height y-mult) (* sector-height (inc y-mult))))))))
 
-(defn process-file [f]
-  (try [f (edge-width-file f)]
+(defn edge-width-file 
+  ([f]
+     (edge-width-file f 1))
+  ([f sectors]
+     (edge-width-img (ImageIO/read (j/as-file f)) sectors)))
+
+(defn process-file [f sectors]
+  (try [f (edge-width-file f sectors)]
        (catch Exception _ nil))) ; I hear it's bad style to filter out unprocessable files using exceptions. I don't care.
 
-(defn process-dir [d]
+(defn process-dir [d sectors]
   (into {}
         (filter identity
-                   (p/pcollect #(process-file (str d "/" %))
+                   (p/pcollect #(process-file (str d "/" %) sectors)
                                (f/list-dir d)))))
 
-(defn list-blurry-images [dir threshold]
-  (m/filter-keys-by-val #(> % threshold) (process-dir dir)))
+(defn sharp? [result threshold sectors]
+  (>= (count (filter #(and (< % threshold)
+                                  (pos? %)) result)) ; don't count sectors with no edges
+      (/ (* sectors sectors) 4)))
+
+(defn list-blurry-images [dir threshold sectors]
+  (m/filter-keys-by-val #(sharp? % threshold sectors)
+                        (process-dir dir)))
 
 (defn -main
   "I don't do a whole lot."
@@ -93,14 +144,19 @@
         (cli args
              ["-h" "--help" "Show help" :default false :flag true]
              ["-t" "--threshold"
-              "Threshold to detect as blurry (default 50)"
+              "Threshold to consider blurry (default 50)"
               :parse-fn #(Integer. %)
-              :default 50])]
+              :default 50]
+             ["-s" "--sectors"
+              "Square root of the number of sectors to split image in to (default 5)"
+              :parse-fn #(Integer. %)
+              :default 5])]
     (when (:help options)
       (println banner)
       (System/exit 0))
     (doall (map println (-> (list-blurry-images (if (f/directory? (first args))
                                                   (first args)
                                                   (str f/*cwd* "/" (first args)))
-                                                (:threshold options))
+                                                (:threshold options)
+                                                (:sectors options))
                             sort)))))
